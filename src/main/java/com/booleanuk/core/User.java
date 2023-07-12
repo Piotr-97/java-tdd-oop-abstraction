b5acad4a4eeb3e2008ba6b206ab4f6d1386faa7b
Piotr Karolak <karolak.piotr.012@gmail.com>
2023-07-12 12:38:13 +0200

    Added class User

--- src/main/java/com/booleanuk/core/User.java
+++ src/main/java/com/booleanuk/core/User.java
@@ -2,5 +2,36 @@ package com.booleanuk.core;
 
 public class User {
 
+    private String email;
+    private String password;
 
+    private boolean isActivated;
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+        isActivated = false;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password)
+
+    public boolean isActivated() {
+        return isActivated;
+    }
+
+    public void setActivated(boolean activated) {
+        isActivated = activated;
+    }
 }
